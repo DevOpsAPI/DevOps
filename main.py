71de9aec009f0a2a8e9bf2f7193a1cc6c648f04e
Simon Ypkemeule <simon.ypkemeule@novi-education.nl>
2024-03-21 14:11:22 +0100

    testen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -38,36 +39,3 @@ async def delete_game(game_id: int):
         return {"error": "Game not found"}
     deleted_game = games_db.pop(game_id)
     return deleted_game
-
-# Voorbeeld van een database als eenpip lijst
-users_db = []
-
-
-# Model voor gebruikersgegevens
-class User:
-    def __init__(self, username: str, email: str):
-        self.username = username
-        self.email = email
-
-
-# Endpoint om een nieuwe gebruiker toe te voegen
-@app.post("/users/")
-def create_user(username: str, email: str):
-    new_user = User(username=username, email=email)
-    users_db.append(new_user)
-    return new_user
-
-
-# Endpoint om een specifieke gebruiker op te halen
-@app.get("/users/{user_id}")
-def read_user(user_id: int):
-    if user_id < 0 or user_id >= len(users_db):
-        raise HTTPException(status_code=404, detail="Gebruiker niet gevonden")
-    return users_db[user_id]
-
-
-# Endpoint om alle gebruikers op te halen
-@app.get("/users/")
-def read_users(skip: int = 0, limit: int = 10):
-    return users_db[skip : skip + limit]
-
