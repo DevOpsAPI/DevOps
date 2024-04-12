bb676d40047687a5ad80a726dd7e25173b0dd287
SenneBolie <sennebolman2005@gmail.com>
2024-04-12 14:21:57 +0200

    endpoints update

--- main.py
+++ main.py
@@ -42,13 +42,10 @@ def create_genre(genre: Genre, db: Session = Depends(get_db)):
     db.refresh(db_genre)
     return db_genre
 
-@app.post("/game_publisher/")
-def create_game_publisher(game_publisher: Game_Publisher, db: Session = Depends(get_db)):
-    db_game_publisher = models.Game_Publisher(game_publisher_name=game_publisher.game_publisher_name)
-    db.add(db_game_publisher)
-    db.commit()
-    db.refresh(db_game_publisher)
-    return db_game_publisher
+@app.get("/genre/")
+def read_genres(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    genres = db.query(models.Genre).offset(skip).limit(limit).all()
+    return genres
 
 @app.delete("/genre/{genre_id}")
 def delete_genre(genre_id: int, db: Session = Depends(get_db)):
@@ -58,3 +55,44 @@ def delete_genre(genre_id: int, db: Session = Depends(get_db)):
     db.delete(db_genre)
     db.commit()
     return db_genre
+
+@app.post("/game_publisher/")
+def create_game_publisher(game_publisher: Game_Publisher, db: Session = Depends(get_db)):
+    db_game_publisher = models.Game_Publisher(game_publisher_name=game_publisher.game_publisher_name)
+    db.add(db_game_publisher)
+    db.commit()
+    db.refresh(db_game_publisher)
+    return db_game_publisher
+
+@app.get("/game_publisher/")
+def read_game_publishers(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    game_publishers = db.query(models.Game_Publisher).offset(skip).limit(limit).all()
+    return game_publishers
+
+@app.delete("/game_publisher/{game_publisher_id}")
+def delete_game_publisher(game_publisher_id: int, db: Session = Depends(get_db)):
+    db_game_publisher = db.query(models.Game_Publisher).filter(models.Game_Publisher.game_publisher_id == game_publisher_id).first()
+    if db_game_publisher is None:
+        raise HTTPException(status_code=404, detail="Game Publisher not found")
+    db.delete(db_game_publisher)
+    db.commit()
+    return db_game_publisher
+
+@app.post("/games/", response_model=Game)
+def create_game(game: Game, db: Session = Depends(get_db)):
+    db_genre = db.query(models.Genre).filter(models.Genre.genre_id == game.genres).first()
+    if db_genre is None:
+        raise HTTPException(status_code=404, detail="Genre not found")
+
+    db_publisher = db.query(models.Game_Publisher).filter(models.Game_Publisher.game_publisher_id == game.game_publishers).first()
+    if db_publisher is None:
+        raise HTTPException(status_code=404, detail="Game Publisher not found")
+
+    db_game = models.Game(title=game.title, genre=db_genre, game_publisher=db_publisher)
+    db.add(db_game)
+    db.commit()
+    db.refresh(db_game)
+    return db_game
+
+
+
