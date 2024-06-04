c37f407175a6a443c93c87b0e3626e12b2252eca
SenneBolie <sennebolman2005@gmail.com>
2024-06-04 12:03:00 +0200

    test

--- main.py
+++ main.py
@@ -6,50 +6,50 @@ import models
 from database import SessionLocal, engine
 from sqlalchemy.orm import Session
 from typing import Optional
-
+ 
 app = FastAPI()
 default_message = "De api werkt!"
 models.Base.metadata.create_all(bind=engine)
-
+ 
 class Genre(BaseModel):
     genre_id: int
     genre_name: str
-
+ 
 class Game_Publisher(BaseModel):
     game_publisher_id: int
     game_publisher_name: str
-
+ 
 class Game(BaseModel):
     title: str
     genres: Optional[Genre] = None
     game_publishers: Optional[Game_Publisher] = None
-
+ 
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
+ 
 @app.get("/")
 def read_root():
     return default_message
-
+ 
 @app.post("/genre/")
 def create_genre(genre: Genre, db: Session = Depends(get_db)):
-    db_genre = models.Genre(genre_name=genre.genre_name)
+    db_genre = models.Genre(genre_id=genre.genre_id,genre_name=genre.genre_name)
     db.add(db_genre)
     db.commit()
     db.refresh(db_genre)
     return db_genre
-
+ 
 @app.get("/genre/{genre_id}")
 def read_genre(genre_id: int, db: Session = Depends(get_db)):
     genre = db.query(models.Genre).filter(models.Genre.genre_id == genre_id).first()
     if genre is None:
         raise HTTPException(status_code=404, detail="Genre not found")
     return genre
-
+ 
 @app.delete("/genre/{genre_id}")
 def delete_genre(genre_id: int, db: Session = Depends(get_db)):
     db_genre = db.query(models.Genre).filter(models.Genre.genre_id == genre_id).first()
@@ -58,22 +58,22 @@ def delete_genre(genre_id: int, db: Session = Depends(get_db)):
     db.delete(db_genre)
     db.commit()
     return db_genre
-
+ 
 @app.post("/game_publisher/")
 def create_game_publisher(game_publisher: Game_Publisher, db: Session = Depends(get_db)):
-    db_game_publisher = models.Game_Publisher(game_publisher_name=game_publisher.game_publisher_name)
+    db_game_publisher = models.Game_Publisher(game_publisher_id=game_publisher.game_publisher_id,game_publisher_name=game_publisher.game_publisher_name)
     db.add(db_game_publisher)
     db.commit()
     db.refresh(db_game_publisher)
     return db_game_publisher
-
+ 
 @app.get("/game_publisher/{game_publisher_id}")
 def read_game_publisher(game_publisher_id: int, db: Session = Depends(get_db)):
     game_publisher = db.query(models.Game_Publisher).filter(models.Game_Publisher.game_publisher_id == game_publisher_id).first()
     if game_publisher is None:
         raise HTTPException(status_code=404, detail="Game Publisher not found")
     return game_publisher
-
+ 
 @app.delete("/game_publisher/{game_publisher_id}")
 def delete_game_publisher(game_publisher_id: int, db: Session = Depends(get_db)):
     db_game_publisher = db.query(models.Game_Publisher).filter(models.Game_Publisher.game_publisher_id == game_publisher_id).first()
@@ -82,23 +82,23 @@ def delete_game_publisher(game_publisher_id: int, db: Session = Depends(get_db))
     db.delete(db_game_publisher)
     db.commit()
     return db_game_publisher
-
+ 
 @app.post("/games/", response_model=Game)
 def create_game(game: Game, db: Session = Depends(get_db)):
     db_genre = db.query(models.Genre).filter(models.Genre.genre_id == game.genres.genre_id).first()
     if db_genre is None:
         raise HTTPException(status_code=404, detail="Genre not found")
-
+ 
     db_publisher = db.query(models.Game_Publisher).filter(models.Game_Publisher.game_publisher_id == game.game_publishers.game_publisher_id).first()
     if db_publisher is None:
         raise HTTPException(status_code=404, detail="Game Publisher not found")
-
+ 
     db_game = models.Game(title=game.title, genres=db_genre, game_publishers=db_publisher)
     db.add(db_game)
     db.commit()
     db.refresh(db_game)
     return db_game
-
+ 
 @app.get("/publisher/games/")
 def get_all_games_by_publisher(db: Session = Depends(get_db)):
     games = db.query(models.Game).all()
@@ -109,7 +109,3 @@ def get_all_games_by_publisher(db: Session = Depends(get_db)):
             if game.game_publishers.game_publisher_id == publisher.game_publisher_id:
                 array.append([game.title, publisher.game_publisher_name])
     return array
-    
-
-
-
--- test_endpoints.py
+++ test_endpoints.py
@@ -7,7 +7,7 @@ from main import app, get_db
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, StaticPool
 from models import Base
-
+ 
  
 client = TestClient(app)
 DATABASE_URL = "sqlite:///:memory:"
@@ -19,25 +19,21 @@ engine = create_engine(
     poolclass=StaticPool,
 )
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+ 
 def override_get_db():
     database = TestingSessionLocal()
     Base.metadata.create_all(bind=engine)
     yield database
     database.close()
-
-
+ 
+ 
 app.dependency_overrides[get_db] = override_get_db
  
-def test_can_call_endpoint():
-    response = client.get("/genre/")
-    assert response.status_code == 200
-
 genre_id = 1
 genre_name = "TestingGenre"
 game_publisher_id = 998
 game_publisher_name = "TestingPublisher"
-
+ 
 def test_create_genres():
     payload = {
     "genre_id": genre_id,
@@ -47,7 +43,12 @@ def test_create_genres():
     data = response.json()
     assert response.status_code == 200
     assert data["genre_name"] == payload["genre_name"]
-
+    assert data["genre_id"] == payload["genre_id"]
+ 
+def test_can_call_endpoint():
+    response = client.get(f"/genre/{genre_id}")
+    assert response.status_code == 200
+ 
 def test_create_game_publishers():
     payload = {
         "game_publisher_id": game_publisher_id,
@@ -57,7 +58,7 @@ def test_create_game_publishers():
     data = response.json()
     assert response.status_code == 200
     assert data["game_publisher_name"] == payload["game_publisher_name"]
-
+ 
 def test_create_games():
     payload = {
             "title": "TestGame",
@@ -68,7 +69,7 @@ def test_create_games():
             "game_publishers": {
             "game_publisher_id": game_publisher_id,
             "game_publisher_name": game_publisher_name
-            
+           
             }
     }
     response = client.post("/games/", json=payload)
@@ -79,7 +80,7 @@ def test_create_games():
     assert data["genres"]["genre_name"] == payload["genres"]["genre_name"]
     assert data["game_publishers"]["game_publisher_id"] == payload["game_publishers"]["game_publisher_id"]
     assert data["game_publishers"]["game_publisher_name"] == payload["game_publishers"]["game_publisher_name"]
-    
+   
  
 def test_delete_game_publisher():
     response = client.delete(f"/game_publisher/{game_publisher_id}")
