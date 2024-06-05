14a10b2a917ba036f43247b6c6d2b75a1ac8c3c5
SenneBolie <sennebolman2005@gmail.com>
2024-06-05 10:06:39 +0200

    Presentatie

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import Session
 from typing import Optional
  
 app = FastAPI()
-default_message = "Hallo"
+default_message = "Presentatie API"
 models.Base.metadata.create_all(bind=engine)
  
 class Genre(BaseModel):
