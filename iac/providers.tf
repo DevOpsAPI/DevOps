a462ae5bfca98d9854b85be71c651dec036c74eb
SenneBolie <sennebolman2005@gmail.com>
2024-06-26 15:43:23 +0200

    terraform 29

--- iac/providers.tf
+++ iac/providers.tf
@@ -1,12 +1,11 @@
 terraform {
     backend "s3" {
     bucket         = "devopsgames"
-    key            = "terraform.tfstate"
+    key            = "terraform"
     region         = "ams3"
     endpoint       = "https://ams3.digitaloceanspaces.com/"
     skip_region_validation = true
     skip_credentials_validation = true
-    skip_requesting_account_id  = true
 }
   required_providers {
     digitalocean = {
